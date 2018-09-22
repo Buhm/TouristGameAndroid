85af19e7c5d9d1723dbae116e871fdcc2878ffac
Buhm <r.lebon@gmail.com>
2018-09-23 01:07:34 +0200

    Ga naar minckeleers Quest save

--- TouristGameAndroid/Resources/Resource.Designer.cs
+++ TouristGameAndroid/Resources/Resource.Designer.cs
@@ -2375,14 +2375,14 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f0800a6
 			public const int aangekomen_markt = 2131230886;
 			
-			// aapt resource value: 0x7f0800c2
-			public const int aangenaam = 2131230914;
+			// aapt resource value: 0x7f0800c5
+			public const int aangenaam = 2131230917;
 			
 			// aapt resource value: 0x7f0800a7
 			public const int aangenaam_herbenus = 2131230887;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int action0 = 2131230896;
+			// aapt resource value: 0x7f0800b3
+			public const int action0 = 2131230899;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2405,17 +2405,17 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f0800ad
-			public const int action_container = 2131230893;
+			// aapt resource value: 0x7f0800b0
+			public const int action_container = 2131230896;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800b4
-			public const int action_divider = 2131230900;
+			// aapt resource value: 0x7f0800b7
+			public const int action_divider = 2131230903;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int action_image = 2131230894;
+			// aapt resource value: 0x7f0800b1
+			public const int action_image = 2131230897;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2432,14 +2432,14 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800c7
-			public const int action_settings = 2131230919;
+			// aapt resource value: 0x7f0800ca
+			public const int action_settings = 2131230922;
 			
-			// aapt resource value: 0x7f0800af
-			public const int action_text = 2131230895;
+			// aapt resource value: 0x7f0800b2
+			public const int action_text = 2131230898;
 			
-			// aapt resource value: 0x7f0800bd
-			public const int actions = 2131230909;
+			// aapt resource value: 0x7f0800c0
+			public const int actions = 2131230912;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2465,6 +2465,9 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08002f
 			public const int beginning = 2131230767;
 			
+			// aapt resource value: 0x7f0800aa
+			public const int bijminckeleers = 2131230890;
+			
 			// aapt resource value: 0x7f080057
 			public const int blocking = 2131230807;
 			
@@ -2474,8 +2477,8 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int cancel_action = 2131230897;
+			// aapt resource value: 0x7f0800b4
+			public const int cancel_action = 2131230900;
 			
 			// aapt resource value: 0x7f080045
 			public const int center = 2131230789;
@@ -2489,8 +2492,8 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800b9
-			public const int chronometer = 2131230905;
+			// aapt resource value: 0x7f0800bc
+			public const int chronometer = 2131230908;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2543,11 +2546,11 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08007e
 			public const int edit_query = 2131230846;
 			
-			// aapt resource value: 0x7f0800c1
-			public const int editemail = 2131230913;
+			// aapt resource value: 0x7f0800c4
+			public const int editemail = 2131230916;
 			
-			// aapt resource value: 0x7f0800c0
-			public const int editname = 2131230912;
+			// aapt resource value: 0x7f0800c3
+			public const int editname = 2131230915;
 			
 			// aapt resource value: 0x7f080092
 			public const int eerder_geweest = 2131230866;
@@ -2555,8 +2558,8 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800bf
-			public const int end_padder = 2131230911;
+			// aapt resource value: 0x7f0800c2
+			public const int end_padder = 2131230914;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2609,11 +2612,14 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800be
-			public const int icon_group = 2131230910;
+			// aapt resource value: 0x7f0800c1
+			public const int icon_group = 2131230913;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
+			
+			// aapt resource value: 0x7f0800a9
+			public const int ikhelp = 2131230889;
 			
 			// aapt resource value: 0x7f08005f
 			public const int image = 2131230815;
@@ -2621,11 +2627,11 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08008d
 			public const int include1 = 2131230861;
 			
-			// aapt resource value: 0x7f0800ab
-			public const int inderdaad = 2131230891;
+			// aapt resource value: 0x7f0800ae
+			public const int inderdaad = 2131230894;
 			
-			// aapt resource value: 0x7f0800ba
-			public const int info = 2131230906;
+			// aapt resource value: 0x7f0800bd
+			public const int info = 2131230909;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2635,6 +2641,9 @@ namespace TouristGameAndroid
 			
 			// aapt resource value: 0x7f080093
 			public const int ja_button = 2131230867;
+			
+			// aapt resource value: 0x7f0800a8
+			public const int kanikhelpen = 2131230888;
 			
 			// aapt resource value: 0x7f080097
 			public const int largeLabel = 2131230871;
@@ -2657,17 +2666,17 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
-			// aapt resource value: 0x7f0800c3
-			public const int loopmeeherbenus = 2131230915;
-			
 			// aapt resource value: 0x7f0800c6
-			public const int masked = 2131230918;
+			public const int loopmeeherbenus = 2131230918;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int media_actions = 2131230899;
+			// aapt resource value: 0x7f0800c9
+			public const int masked = 2131230921;
 			
-			// aapt resource value: 0x7f0800c4
-			public const int message = 2131230916;
+			// aapt resource value: 0x7f0800b6
+			public const int media_actions = 2131230902;
+			
+			// aapt resource value: 0x7f0800c7
+			public const int message = 2131230919;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2696,23 +2705,23 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800bc
-			public const int notification_background = 2131230908;
+			// aapt resource value: 0x7f0800bf
+			public const int notification_background = 2131230911;
 			
-			// aapt resource value: 0x7f0800b6
-			public const int notification_main_column = 2131230902;
+			// aapt resource value: 0x7f0800b9
+			public const int notification_main_column = 2131230905;
 			
-			// aapt resource value: 0x7f0800b5
-			public const int notification_main_column_container = 2131230901;
+			// aapt resource value: 0x7f0800b8
+			public const int notification_main_column_container = 2131230904;
 			
-			// aapt resource value: 0x7f0800ac
-			public const int ok_button1 = 2131230892;
+			// aapt resource value: 0x7f0800af
+			public const int ok_button1 = 2131230895;
 			
-			// aapt resource value: 0x7f0800aa
-			public const int ok_button2 = 2131230890;
+			// aapt resource value: 0x7f0800ad
+			public const int ok_button2 = 2131230893;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int owja = 2131230888;
+			// aapt resource value: 0x7f0800ab
+			public const int owja = 2131230891;
 			
 			// aapt resource value: 0x7f08004c
 			public const int parallax = 2131230796;
@@ -2741,11 +2750,11 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800bb
-			public const int right_icon = 2131230907;
+			// aapt resource value: 0x7f0800be
+			public const int right_icon = 2131230910;
 			
-			// aapt resource value: 0x7f0800b7
-			public const int right_side = 2131230903;
+			// aapt resource value: 0x7f0800ba
+			public const int right_side = 2131230906;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2849,8 +2858,8 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800b2
-			public const int status_bar_latest_event_content = 2131230898;
+			// aapt resource value: 0x7f0800b5
+			public const int status_bar_latest_event_content = 2131230901;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2891,8 +2900,8 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800b8
-			public const int time = 2131230904;
+			// aapt resource value: 0x7f0800bb
+			public const int time = 2131230907;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2939,11 +2948,11 @@ namespace TouristGameAndroid
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800c5
-			public const int visible = 2131230917;
+			// aapt resource value: 0x7f0800c8
+			public const int visible = 2131230920;
 			
-			// aapt resource value: 0x7f0800a9
-			public const int weet_het_weer = 2131230889;
+			// aapt resource value: 0x7f0800ac
+			public const int weet_het_weer = 2131230892;
 			
 			// aapt resource value: 0x7f080091
 			public const int welkom = 2131230865;
@@ -3146,79 +3155,85 @@ namespace TouristGameAndroid
 			public const int herbenus_quest1 = 2130968620;
 			
 			// aapt resource value: 0x7f04002d
-			public const int kommee_intro = 2130968621;
+			public const int herbenus_quest1a = 2130968621;
 			
 			// aapt resource value: 0x7f04002e
-			public const int mooiestad_intro = 2130968622;
+			public const int herbenus_quest_gaminckeleers = 2130968622;
 			
 			// aapt resource value: 0x7f04002f
-			public const int nietherinneren_main = 2130968623;
+			public const int kommee_intro = 2130968623;
 			
 			// aapt resource value: 0x7f040030
-			public const int notification_action = 2130968624;
+			public const int mooiestad_intro = 2130968624;
 			
 			// aapt resource value: 0x7f040031
-			public const int notification_action_tombstone = 2130968625;
+			public const int nietherinneren_main = 2130968625;
 			
 			// aapt resource value: 0x7f040032
-			public const int notification_media_action = 2130968626;
+			public const int notification_action = 2130968626;
 			
 			// aapt resource value: 0x7f040033
-			public const int notification_media_cancel_action = 2130968627;
+			public const int notification_action_tombstone = 2130968627;
 			
 			// aapt resource value: 0x7f040034
-			public const int notification_template_big_media = 2130968628;
+			public const int notification_media_action = 2130968628;
 			
 			// aapt resource value: 0x7f040035
-			public const int notification_template_big_media_custom = 2130968629;
+			public const int notification_media_cancel_action = 2130968629;
 			
 			// aapt resource value: 0x7f040036
-			public const int notification_template_big_media_narrow = 2130968630;
+			public const int notification_template_big_media = 2130968630;
 			
 			// aapt resource value: 0x7f040037
-			public const int notification_template_big_media_narrow_custom = 2130968631;
+			public const int notification_template_big_media_custom = 2130968631;
 			
 			// aapt resource value: 0x7f040038
-			public const int notification_template_custom_big = 2130968632;
+			public const int notification_template_big_media_narrow = 2130968632;
 			
 			// aapt resource value: 0x7f040039
-			public const int notification_template_icon_group = 2130968633;
+			public const int notification_template_big_media_narrow_custom = 2130968633;
 			
 			// aapt resource value: 0x7f04003a
-			public const int notification_template_lines_media = 2130968634;
+			public const int notification_template_custom_big = 2130968634;
 			
 			// aapt resource value: 0x7f04003b
-			public const int notification_template_media = 2130968635;
+			public const int notification_template_icon_group = 2130968635;
 			
 			// aapt resource value: 0x7f04003c
-			public const int notification_template_media_custom = 2130968636;
+			public const int notification_template_lines_media = 2130968636;
 			
 			// aapt resource value: 0x7f04003d
-			public const int notification_template_part_chronometer = 2130968637;
+			public const int notification_template_media = 2130968637;
 			
 			// aapt resource value: 0x7f04003e
-			public const int notification_template_part_time = 2130968638;
+			public const int notification_template_media_custom = 2130968638;
 			
 			// aapt resource value: 0x7f04003f
-			public const int npcsjeng_intro = 2130968639;
+			public const int notification_template_part_chronometer = 2130968639;
 			
 			// aapt resource value: 0x7f040040
-			public const int select_dialog_item_material = 2130968640;
+			public const int notification_template_part_time = 2130968640;
 			
 			// aapt resource value: 0x7f040041
-			public const int select_dialog_multichoice_material = 2130968641;
+			public const int npcsjeng_intro = 2130968641;
 			
 			// aapt resource value: 0x7f040042
-			public const int select_dialog_singlechoice_material = 2130968642;
+			public const int select_dialog_item_material = 2130968642;
 			
 			// aapt resource value: 0x7f040043
-			public const int sjengintroherbenus_quest = 2130968643;
+			public const int select_dialog_multichoice_material = 2130968643;
 			
 			// aapt resource value: 0x7f040044
-			public const int support_simple_spinner_dropdown_item = 2130968644;
+			public const int select_dialog_singlechoice_material = 2130968644;
 			
 			// aapt resource value: 0x7f040045
-			public const int tooltip = 2130968645;
+			public const int sjengintroherbenus_quest = 2130968645;
+			
+			// aapt resource value: 0x7f040046
+			public const int support_simple_spinner_dropdown_item = 2130968646;
+			
+			// aapt resource value: 0x7f040047
+			public const int tooltip = 2130968647;
 			
 			static Layout()
 			{
